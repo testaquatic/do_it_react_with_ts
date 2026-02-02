7608c5dd6e00049e3b9e41563b9a0aaa3805ef5b
aquatictest <aquatictest@proton.me>
2026-02-02 10:24:17 +0900

    1장 종료

---
+++ ch01/01_5_first_react_project/src/App.tsx
@@ -0,0 +1,15 @@
+import * as D from './data'
+
+function App() {
+  return (
+    <div>
+      <p>
+        {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
+      </p>
+      <img src={D.randomAvatar()} height={50} />
+      <img src={D.randomImage()} height={300} />
+    </div>
+  )
+}
+
+export default App
---
+++ ch01/01_5_first_react_project/src/data/index.ts
@@ -0,0 +1,4 @@
+export * from './util'
+export * from './chance'
+export * from './image'
+export * from './date'
